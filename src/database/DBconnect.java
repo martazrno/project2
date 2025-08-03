a6e9016dbe8ed2f0ee8332e1e99aa2a3821c783a
Soma <soma.nagy23@gmail.com>
2025-08-03 19:00:00 +0200

    asd

--- src/database/DBconnect.java
+++ src/database/DBconnect.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 public class DBconnect {
     private static final String URL = "jdbc:postgresql://localhost:5432/postgres";
     private static final String USER = "postgres";
-    private static final String PASSWORD = "0000";
+    private static final String PASSWORD = "groupfive";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);}}
